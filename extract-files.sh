28d9edc85432ed1ef8beffbaf42073c0ffdfc795
luk1337 <priv.luk@gmail.com>
2023-08-21 00:11:44 +0800

    messi: oplus fingerprint hack, since we are building 2.3, which missing blob dependencies - might.
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- extract-files.sh
+++ extract-files.sh
@@ -53,6 +53,17 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup() {
+    case "${1}" in
+        odm/bin/hw/vendor.oplus.hardware.biometrics.fingerprint@2.1-service_uff)
+            grep -q libshims_fingerprint.oplus.so "${2}" || "${PATCHELF}" --add-needed libshims_fingerprint.oplus.so "${2}"
+            ;;
+        odm/etc/vintf/manifest/manifest_oplus_fingerprint.xml)
+            sed -ni "/android.hardware.biometrics.fingerprint/{x;s/hal format/hal override=\"true\" format/;x};x;1!p;\${x;p}" "${2}"
+            ;;
+    esac
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
