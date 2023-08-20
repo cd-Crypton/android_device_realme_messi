6a64d1947a6ba7c0ef0899197ce70d9788d68c4e
Carlo Dee <carlodee.official@proton.me>
2023-08-20 14:44:29 +0800

    messi: add hardware/oplus as one of the soong_namespaces. * Also, include sepolicy from oplus.
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- device.mk
+++ device.mk
@@ -109,7 +109,8 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/oplus
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/realme/messi/messi-vendor.mk)
