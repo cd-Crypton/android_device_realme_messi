8b1ada2ab6e9845df20dee3b6c051fc9cdd68d1a
Carlo Dee <carlodee.official@proton.me>
2023-08-21 11:52:45 +0800

    messi: rename rro overlay for CarrierConfig back to -ResCommon * Since it was extracted from CarrierConfigResCommon_sys.apk, had to rename it back - yeah, I can just revert but anyway...
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- device.mk
+++ device.mk
@@ -207,7 +207,7 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
-    CarrierConfigOverlay \
+    CarrierConfigResCommon \
     WifiResTarget
 
 # Partitions
