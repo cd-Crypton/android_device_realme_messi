15c6efbbc5368ed49dc78b19c7ef1d222b72129d
Alexander Koskovich <akoskovich@pm.me>
2023-08-21 08:31:22 +0800

    messi: Change GPS stock proxy app to google/aosp. * Include GPS packages as well to support changes.
    Note: This was the default in CAF before Realme changed it to com.coloros.locationproxy. This is apart of the framework 'oplus-lbs-services', which we don't use in Lineage.
    Signed-off-by: Carlo Dee <carlodee.official@proton.me>

--- device.mk
+++ device.mk
@@ -166,6 +166,12 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss.visibility_control@1.0.vendor \
     android.hardware.gnss@2.1.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/gps.conf:$(TARGET_COPY_OUT_ODM)/etc/gps.conf
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.location.gps.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.location.gps.xml
+
 # HIDL
 PRODUCT_PACKAGES += \
     libhidltransport.vendor \
